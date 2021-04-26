8f334975efaa396970bcff3aa62de4ccb4f47e35
Pyataev Sergey <pyataev.serge@outlook.com>
2021-04-26 18:46:39 +0400

    change vm template

--- terraform/main.tf
+++ terraform/main.tf
@@ -49,7 +49,7 @@ resource "google_container_node_pool" "primary_nodes" {
     }
 
     preemptible  = true
-    machine_type = "e2-standard-4"
+    machine_type = "e2-standard-2"
     tags         = ["gke-node", "${var.project_id}-gke"]
     metadata = {
       disable-legacy-endpoints = "true"
